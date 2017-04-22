081f621e5e5f331c1e76b1795a33304ec40f7296
shibo <shaaban.altanany@gmail.com>
2017-04-22 21:35:13 +0200

    first commit

---
+++ src/main/java/com/mycompany/boot/NewClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author shibo
+ */
+@SpringBootApplication
+public class NewClass {
+    public static void main(String[] args) {
+        SpringApplication.run(NewClass.class, args);
+    }
+}
---
+++ src/main/java/com/mycompany/boot/controller.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.boot;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author shibo
+ */
+@RestController
+public class controller {
+    @RequestMapping("/hi")
+    public String sayHI(){
+    
+    return "hi";}
+}
